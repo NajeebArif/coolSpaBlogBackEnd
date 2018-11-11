e2ad54a0444b79e58031afea09cafc5fd8a8c8b4
NajeebArif <NajeebArif786@outlook.com>
2018-11-12 00:48:46 +0530

    refactoring the code

--- src/main/java/narif/pocs/coolspablog/controllers/WelcomePageController.java
+++ src/main/java/narif/pocs/coolspablog/controllers/WelcomePageController.java
@@ -16,28 +16,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//@CrossOrigin(origins="http://localhost:4200/",allowCredentials="false")
 @RestController
 public class WelcomePageController {
-	
+
 	@RequestMapping("/user")
-	  public Principal user(Principal user) {
-	    return user;
-	  }
-	
-	@RequestMapping(value="/j_spring_security_logout", method = RequestMethod.GET)
-    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null){
-            new SecurityContextLogoutHandler().logout(request, response, auth);
-        }
-        return "{}";//You can redirect wherever you want, but generally it's a good practice to show login screen again.
-    }
-	
+	public Principal user(Principal user) {
+		return user;
+	}
+
+	@RequestMapping(value = "/j_spring_security_logout", method = RequestMethod.GET)
+	public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "{}";// return the json object so as angular does not throw any parsing error
+	}
+
 	@GetMapping("/api")
-	public Map<String, Object> getTheWelcomeNotes(){
+	public Map<String, Object> getTheWelcomeNotes() {
 		Map<String, Object> model = new HashMap<>();
-		model.put("id",UUID.randomUUID().toString());
+		model.put("id", UUID.randomUUID().toString());
 		model.put("content", "Welcome to the coolest SPA Blog site ever");
 		return model;
 	}
